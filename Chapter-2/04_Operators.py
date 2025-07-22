9ffcaace62c624e7d6bf13051dd79d6aa81d8596
Jagadish-98 <mohantyj37@gmail.com>
2025-07-22 21:04:09 +0530

    Code chsnges to as usual.

--- Chapter-2/04_Operators.py
+++ Chapter-2/04_Operators.py
@@ -21,10 +21,10 @@ print(e)
 e=True or False 
 
 # Truth table of 'or'
-# print("True or False is", True or False)
-# print("True or True is", True or True)
-# print("False or True is" ,False or True)
-# print("False or False is", False or False)
+print("True or False is", True or False)
+print("True or True is", True or True)
+print("False or True is" ,False or True)
+print("False or False is", False or False)
 
 # Truth table of 'and'
 print("True and False is", True and False)
